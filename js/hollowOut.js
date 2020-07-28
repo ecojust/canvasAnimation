5c0353fb167fde43062e1f14a22ee45490d48cb7
ecojust <995674251@qq.com>
2020-07-29 00:09:48 +0800

    多重波浪线条

--- js/hollowOut.js
+++ js/hollowOut.js
@@ -6,12 +6,15 @@
 # Description: 
 #
 ============================================================================= */
-var uniformTime = 0,bindBuffer = false,n = 0;
+var uniformTime = 0.3,bindBuffer = false,n = 0;
 function hollowOut() {
     const vs = `
         attribute vec4 a_Position;
+        attribute vec2 a_TexCoord;
+        varying vec2 v_TexCoord;
         void main(){
             gl_Position = a_Position;
+            v_TexCoord = a_TexCoord;
         }
     `;
     const fs = `
@@ -20,15 +23,47 @@ function hollowOut() {
         #endif
         uniform float time;
         uniform sampler2D u_Sampler;
+        varying vec2 v_TexCoord;
+
         void main(){
-          vec2 p = gl_FragCoord.xy;
-          vec2 uv = gl_FragCoord.xy/vec2(2880.,1080.);
-          vec4 tex = texture2D(u_Sampler,uv);
-          vec4 color = vec4(1.0,0.0,0.0,0.0);
-          if(p.x > 10.){
-            color = vec4(0.0,1.0,0.0,0.0);
+          // vec2 p = gl_FragCoord.xy;
+          // vec2 uv = gl_FragCoord.xy/vec2(800.,600.);
+
+
+          vec4 tex = texture2D(u_Sampler,v_TexCoord);
+
+
+          vec4 color = vec4(1.0,0.0,0.0,1.0);
+          if(v_TexCoord.y > 0.5){
+            color = vec4(0.0,1.0,0.0,1.0);
           }
-          gl_FragColor = color;
+
+          float cycle = sin(time * 5.0);
+
+          vec4 line = vec4(0.0);
+
+          float index = 1.0;
+          for(int i = 1; i < 20; i++){
+            float sy = sin((v_TexCoord.x + 0.0) * 9.0  + cycle ) * 0.4 * (cycle - index/150.0) + 0.5 ;
+            float dis = abs(sy - v_TexCoord.y);
+            if(dis < 0.005){
+              float alpha = smoothstep(1.0,0.0,dis/0.005);
+              line = vec4(vec3(0.0,0.0,1.0) * alpha,alpha);
+            }
+
+            index +=1.0;
+          }
+
+
+          
+
+
+          
+          // gl_FragColor = mix(tex,color,0.5) + line;
+          gl_FragColor = mix(tex,line,0.5);
+          // gl_FragColor = line;
+
+
         }
     `;
     var canvas = document.getElementById("webgl");
@@ -81,6 +116,16 @@ function hollowOut() {
         gl.enableVertexAttribArray(a_Position);
 
 
+        var a_TexCoord = gl.getAttribLocation(gl.program,'a_TexCoord');
+        if (a_TexCoord < 0) {
+            console.log('Failed to get the storage location of a_TexCoord');
+            return -1;
+        }
+        //将缓冲区对象分配给a_TexCoord变量并开启访问
+        gl.vertexAttribPointer(a_TexCoord,2,gl.FLOAT,false,fsize * 4,fsize * 2);
+        gl.enableVertexAttribArray(a_TexCoord);
+
+
 
         var time = gl.getUniformLocation(gl.program, "time");
         if (time < 0) {
@@ -108,6 +153,8 @@ function hollowOut() {
           }
 
           image.src = '../images/map.jpg';
+          // image.src = '../images/map2.jpeg';
+
 
           return true;
       }
@@ -134,18 +181,27 @@ function hollowOut() {
         //将0号纹理传递给着色器
         gl.uniform1i(u_Sampler,0);
 
-        draw();
+        animate();
       }
 
-      function draw(){
+      function animate(){
         gl.clearColor(0.4, 0.5, 0.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.enable(gl.BLEND);
+        // gl.enable(gl.BLEND);
         // gl.blendFunc(gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-      }
 
-      // requestAnimationFrame(draw);
+        uniformTime+=0.001;
+        // console.log(uniformTime)
+        var time = gl.getUniformLocation(gl.program, "time");
+        if (time < 0) {
+          console.log("Failed to get the storage location of a_Position");
+          return -1;
+        }
+        gl.uniform1f(time, uniformTime);
+        requestAnimationFrame(animate);
+
+      }
     }
     main();
   }
